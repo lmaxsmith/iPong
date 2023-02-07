23e58f8b880cbc2f6e0a949f25ba88310b4a9a6f
Logan Smith <lmaxsmith@gmail.com>
2023-02-06 22:40:40 -0800

    simple opponent adjustments

--- iPong Unity/Assets/SimpleOpponent.cs
+++ iPong Unity/Assets/SimpleOpponent.cs
@@ -1,12 +1,17 @@
 using System;
 using Argyle.UnclesToolkit.Base;
+using Argyle.UnclesToolkit.Jobs;
+using Cysharp.Threading.Tasks;
 using Unity.VisualScripting;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
 
 	public class SimpleOpponent : ArgyleComponent
 	{
+
+		#region ==== Settings ====------------------
 
 		private Ball _ball;
 
@@ -22,18 +27,68 @@ namespace DefaultNamespace
 		}
 
 		public Paddle _paddle;
+		public float threshold = .1f;
 		
-		private void Update()
+		
+		#region == name ==----
+
+		public float _loopDuration = .5f;
+
+		private Looper controllLoop = new Looper("SimpleAIControlLoop", 1);
+
+		#endregion ----/name ==
+
+		#endregion -----------------/Settings ====
+
+
+		#region ==== Monobehavior ====------------------
+
+		protected override void PostStart()
+		{
+			base.PostStart();
+			controllLoop.LoopDuration = _loopDuration;
+			controllLoop.Add(Control);
+		}
+
+		private void OnEnable()
+		{
+			controllLoop.StartLoop();
+		}
+
+		private void OnDisable()
+		{
+			controllLoop.StopLoop();
+		}
+
+		#endregion -----------------/Monobehavior ====
+		
+		
+		private async UniTask Control()
 		{
 			if (Ball)
 			{
-				if(Ball.TForm.position.x < _paddle.TForm.position.x)
-					_paddle.SetPaddleMovement(-1);
-				else if(Ball.TForm.position.x > _paddle.TForm.position.x)
-					_paddle.SetPaddleMovement(1);
-				else
+				if (IsOutOfThreshold())
+				{
+					if(isLeft())
+					{
+						_paddle.SetPaddleMovement(-1);
+						while (!isRight())
+							await UniTask.Delay(100);
+					}					
+					else if (isRight())
+					{
+						_paddle.SetPaddleMovement(1);
+						while (!isLeft())
+							await UniTask.Delay(100);
+					}			
+					
 					_paddle.SetPaddleMovement(0);
+				}
 			}
 		}
+
+		public bool IsOutOfThreshold() => Mathf.Abs(Ball.TForm.position.x - _paddle.TForm.position.x) > threshold;
+		public bool isLeft() => Ball.TForm.position.x < _paddle.TForm.position.x - threshold;
+		public bool isRight() => Ball.TForm.position.x > _paddle.TForm.position.x + threshold;
 	}
 }
