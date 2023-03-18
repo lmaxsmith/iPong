ada4a356012254259ed9c85886d9aec690847ad3
Logan Smith <lmaxsmith@gmail.com>
2023-03-17 22:39:30 -0700

    New version made by gpt4

--- iPong Unity/Assets/PyTorchControl.cs
+++ iPong Unity/Assets/PyTorchControl.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using EasyButtons;
 using Unity.Barracuda;
@@ -19,52 +21,65 @@ namespace DefaultNamespace
 			base.PostStart();
 			LoadModel();
 		}
-
-		[Button]
+		
 		public void LoadModel()
 		{
 			model = ModelLoader.Load(nnModel);
 			engine = WorkerFactory.CreateWorker(model);
-
-
 		}
 
-		[Button]
-		public void RunModel()
-		{
-			var input = _dataManager.LastData.ToTensor();
-			var output = engine.Execute(input).PeekOutput();
-			
-			Debug.Log($"Model Output: {output}");
-		}
+		
+
+		public List<float> inputArray = new List<float>();
+		public List<float> outputArray = new List<float>();
+		public int direction = 0;
 
 		protected override async UniTask Control()
 		{
 			using var input = _dataManager.LastData.ToTensor();
 			{
 				var inputArr = input.ToReadOnlyArray();
+				inputArray = inputArr.ToList();
 				Debug.Log($"Model input: {inputArr}");
 				var execution = engine.Execute(input);
-				var output = execution.PeekOutput();
-				var arr = output.ToReadOnlyArray();
 				
-				int direction;
-				
-				
-				if (arr[0] > arr[1] && arr[0] > arr[2])
+				float[] softmax;
+				using var output = execution.PeekOutput();
+				{
+					var arr = output.ToReadOnlyArray();
+					softmax = SoftMax(arr);
+				}
+				outputArray = softmax.ToList();
+
+				if (softmax[0] > softmax[1] && softmax[0] > softmax[2])
 					direction = 1;
-				else if (arr[1] > arr[0] && arr[1] > arr[2])
+				else if (softmax[1] > softmax[0] && softmax[1] > softmax[2])
 					direction = -1;
 				else
 					direction = 0;
 				
-				
-				
-				_paddle.SetPaddleMovement(direction = 0);
+				_paddle.SetPaddleMovement(direction);
 			
-				Log($"Model output: {arr[0]}, {arr[1]}, {arr[2]}: direction: {direction}");
+				Log($"Model output: {softmax[0]}, {softmax[1]}, {softmax[2]}: direction: {direction}");
 			}
 			
+		}
+		
+		
+		static float[] SoftMax(float[] inputs)
+		{
+			float[] result = new float[inputs.Length];
+			float sum = 0;
+			for (int i = 0; i < inputs.Length; i++)
+			{
+				result[i] = Mathf.Exp(inputs[i]);
+				sum += result[i];
+			}
+			for (int i = 0; i < inputs.Length; i++)
+			{
+				result[i] /= sum;
+			}
+			return result;
 		}
 	}
 }
